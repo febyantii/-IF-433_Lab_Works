595d296dea67cbf532b030f357f6651d64eb7c5c
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-02-06 15:43:05 +0700

    week01: refactor to use val and string templates

--- src/oop_115019_FebyantiEirene/week01/GradingSystem.kt
+++ src/oop_115019_FebyantiEirene/week01/GradingSystem.kt
@@ -1,9 +1,9 @@
 package oop_115019_FebyantiEirene.week01
 
 fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
+    val name: String = "John Thor"
+    val score: Int = 80
 
     //concatenation gaya lama
-    println("Nama: " + name + ", Nilai:" + score)
+    println("Nama: $name, Nilai: $score")
 }
