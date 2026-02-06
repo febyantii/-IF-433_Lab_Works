e7b3cef40a414b953e20e6e130304557f92a1d8a
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-02-06 15:40:20 +0700

    week01: initial setup and procedural code

---
+++ src/oop_115019_FebyantiEirene/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_115019_FebyantiEirene.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    //concatenation gaya lama
+    println("Nama: " + name + ", Nilai:" + score)
+}
