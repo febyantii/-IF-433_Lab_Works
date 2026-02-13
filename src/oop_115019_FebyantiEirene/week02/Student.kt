7002e738005bc783b219f702c6edba02eeea7ffe
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-02-13 15:25:28 +0700

    week02: add secondary constructor

--- src/oop_115019_FebyantiEirene/week02/Student.kt
+++ src/oop_115019_FebyantiEirene/week02/Student.kt
@@ -1,6 +1,11 @@
 package oop_115019_FebyantiEirene.week02
 
 class Student (val name: String, val nim: String, var major: String){
+
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+
     init {
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid")
