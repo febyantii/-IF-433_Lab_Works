ae7e43e787cbd2ea2e3a1d43bcef69599da38f2f
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-02-13 16:20:06 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_115019_FebyantiEirene/week02/game.kt
@@ -0,0 +1,17 @@
+package oop_115019_FebyantiEirene.week02
+
+class Hero (val name: String, var baseDamage: Int, var hp: Int = 100){
+    fun attack (targetName: String) {
+        println("$name menebas $targetName! ")
+    }
+    fun takeDamage(damage: Int) {
+        if (hp <= 0){
+            hp = 0
+        } else {
+            hp -= damage
+        }
+    }
+    fun isAlive() : Boolean{
+        return hp > 0
+    }
+}
