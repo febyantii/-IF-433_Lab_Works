252eefde057b2d64a73b33892ba5d561f414fea8
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-02-20 15:04:36 +0700

    week03: cleanup code

--- src/oop_115019_FebyantiEirene/week03/Main.kt
+++ src/oop_115019_FebyantiEirene/week03/Main.kt
@@ -2,8 +2,6 @@ package oop_115019_FebyantiEirene.week03
 
 fun main() {
     val e = Employee("Budi")
-    //println("--- Test Error ---")
-    //e.salary = 5000 //Ini akan meledakkan program lol
 
     //1. Test validasi salary
     e.salary = -100 //harusnya print error
@@ -12,7 +10,6 @@ fun main() {
 
     //2. Test Encapsulation
     e.increasePerformance()
-    // e.performanceRating = 5 //coba uncomment, pasti Merah (Error)
 
     //3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
