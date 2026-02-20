57f73555391e7925330b572a1a8fa192bf93297b
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-02-20 14:45:35 +0700

    week03: simulate stack overflow error in setter

--- src/oop_115019_FebyantiEirene/week03/Employee.kt
+++ src/oop_115019_FebyantiEirene/week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_115019_FebyantiEirene.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            //Kode ini berbahaya: memanggil setter di dalam setter
+            this.salary = value
+        }
 }
---
+++ src/oop_115019_FebyantiEirene/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_115019_FebyantiEirene.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000 //Ini akan meledakkan program lol
+}
