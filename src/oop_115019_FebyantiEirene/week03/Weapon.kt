14f509e23e6e50f9304f2e7737867ada4a9fde9c
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-02-20 15:12:42 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_115019_FebyantiEirene/week03/Weapon.kt
@@ -0,0 +1,19 @@
+package oop_115019_FebyantiEirene.week03
+
+class Weapon (val name: String) {
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Damage Value negatif!")
+                field = value
+            } else if (value > 1000) {
+                field = value
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = if (damage > 800) "Legendary" else if (damage > 500) "Epic" else "Common"
+
+}
