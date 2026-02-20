882145c4f363d478d5020947a03c63256766994b
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-02-20 15:33:18 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_115019_FebyantiEirene/week03/Player.kt
@@ -0,0 +1,22 @@
+package oop_115019_FebyantiEirene.week03
+
+class Player(val username: String) {
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp/100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount <= 0) {
+            println("XP harus positif.")
+            return
+        }
+
+        val levelSebelumnya = level
+
+        xp += amount
+        if (level > levelSebelumnya) {
+            println("Level Up! Selamat $username naik ke level $level")
+        }
+    }
+}
