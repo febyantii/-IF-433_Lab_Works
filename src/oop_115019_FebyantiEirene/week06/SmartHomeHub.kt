4907268db54896bf3127bc3ef1fc13413fccf9b1
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-03-13 15:25:31 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_115019_FebyantiEirene/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_115019_FebyantiEirene.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
