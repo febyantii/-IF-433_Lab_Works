0701069ce10727bddb53e57d5793594d80d572e7
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-03-27 15:22:31 +0700

    week07: (task) implement exhaustive processEvent

---
+++ src/oop_115019_FebyantiEirene/week07/GameExecutor.kt
@@ -0,0 +1,21 @@
+package oop_115019_FebyantiEirene.week07
+
+fun processEvent(event: BattleState) {
+    when (event) {
+        is BattleState.MonsterEncounter -> {
+            // Smart Cast: Mengakses properti monsterName
+            println("Awas! Kamu bertemu dengan monster: ${event.monsterName}")
+        }
+
+        is BattleState.LootDropped -> {
+            // Destructuring: Mengambil data dari properti item (GameItem)
+            val item = event.item
+            println("Hoki! Monster menjatuhkan [${item.rarity}] ${item.name} (Damage: ${item.damage})")
+        }
+
+        BattleState.SafeZone -> {
+            // Karena SafeZone adalah 'object', tidak perlu menggunakan 'is'
+            println("Kamu berada di Area Aman. Waktunya beristirahat.")
+        }
+    }
+}
