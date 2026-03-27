a4ea30d9433864f111b0ba505ed300c8b44c626e
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-03-27 15:07:03 +0700

    week07: (task) create GameItem data class

--- src/oop_115019_FebyantiEirene/week07/GameModels.kt
+++ src/oop_115019_FebyantiEirene/week07/GameModels.kt
@@ -3,3 +3,5 @@ package oop_115019_FebyantiEirene.week07
 enum class ItemRarity(val dropChance: Int) {
     COMMON(70), UNCOMMON(20), RARE(7), EPIC(2), LEGENDARY(1)
 }
+
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity)
