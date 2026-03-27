24196e8963a3403a3aa80183af530c62d49fd7b4
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-03-27 15:16:13 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_115019_FebyantiEirene/week07/BattleEvent.kt
@@ -0,0 +1,7 @@
+package oop_115019_FebyantiEirene.week07
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    object SafeZone : BattleState()
+}
