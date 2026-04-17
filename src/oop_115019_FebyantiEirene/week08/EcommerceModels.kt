1b522f0af4bfc3701b20450f7ff0a5b96585b53f
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-04-17 15:29:07 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_115019_FebyantiEirene/week08/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_115019_FebyantiEirene.week08
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
