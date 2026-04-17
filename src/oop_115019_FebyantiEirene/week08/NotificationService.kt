6d0cf6956360873aa2c9a04efdc4e408a2a13009
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-04-17 14:55:19 +0700

    week08: implement smart cast in function

---
+++ src/oop_115019_FebyantiEirene/week08/NotificationService.kt
@@ -0,0 +1,15 @@
+package oop_115019_FebyantiEirene.week08
+
+class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile){
+        if (user.email != null) {
+            sendEmail(user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
