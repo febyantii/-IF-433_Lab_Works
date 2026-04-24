4d01ca3b4fc6792a6a3583ea28d2a8f3a651c9b7
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-04-24 13:45:24 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_115019_FebyantiEirene/week09/TradingDashboard.kt
@@ -0,0 +1,17 @@
+package oop_115019_FebyantiEirene.week09
+
+// File: TradingDashboard.kt
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),   // Profit, CLOSED
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),  // Loss, CLOSED
+        TradeLog("BTCUSDT", "LONG", 50, 120.0, "OPEN"),    // Profit, OPEN
+        TradeLog("SOLUSDT", "SHORT", 5, -12.0, "OPEN"),    // Loss, OPEN (Koin ke-3)
+        TradeLog("ETHUSDT", "LONG", 10, 25.0, "CLOSED"),   // Profit, CLOSED
+        TradeLog("BTCUSDT", "SHORT", 25, -45.5, "CLOSED")  // Loss, CLOSED
+    )
+
+    // Opsional: Print untuk memastikan data masuk
+    println("Berhasil memuat ${tradeHistory.size} data trading.")
+}
