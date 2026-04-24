444f6405acc47aac2a830c7b535c79dd6e4b9efa
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-04-24 13:15:12 +0700

    week09: implement map HOF for data transformation

--- src/oop_115019_FebyantiEirene/week09/LamdaPractice.kt
+++ src/oop_115019_FebyantiEirene/week09/LamdaPractice.kt
@@ -1,2 +1,10 @@
 package oop_115019_FebyantiEirene.week09
 
+fun main() {
+    println("=== TEST LAMBDA ===")
+    val sumLambda = { a: Int, b: Int -> a + b }
+    println("hasil SUm: ${sumLambda(5, 10)}")
+
+    val squareImplicit: (Int) -> Int = { it * it }
+    println("Hasil Square: ${squareImplicit(4)}")
+}
