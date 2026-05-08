9cab81d4c2f3e99c82b09ce2d8e9ee6c033faa5d
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-05-08 14:54:40 +0700

    week10: implement basic generic function printData

---
+++ src/oop_115019_FebyantiEirene/week10/GenericMethods.kt
@@ -0,0 +1,11 @@
+package oop_001_johnthor.week10
+
+// Fungsi cetak generik
+fun <T> printData(data: T) {
+    println("Data yang diterima: $data")
+}
+
+// Fungsi dengan tipe kembalian generik
+fun <T> processData(input: T): T {
+    return input
+}
