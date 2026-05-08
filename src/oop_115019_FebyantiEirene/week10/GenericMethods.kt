ecc8e0ea42cc396ab0d11414e77b61a0fd144c4c
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-05-08 14:55:01 +0700

    week10: implement generic function with return type

--- src/oop_115019_FebyantiEirene/week10/GenericMethods.kt
+++ src/oop_115019_FebyantiEirene/week10/GenericMethods.kt
@@ -2,7 +2,7 @@ package oop_001_johnthor.week10
 
 // Fungsi cetak generik
 fun <T> printData(data: T) {
-    println("Data yang diterima: $data")
+    println("Data yang diterima : $data")
 }
 
 // Fungsi dengan tipe kembalian generik
