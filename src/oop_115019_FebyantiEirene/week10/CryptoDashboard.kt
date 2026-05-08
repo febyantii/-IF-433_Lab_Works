1c9f4ac4cd77c69b6512c572678fbc73c318097d
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-05-08 15:02:41 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_115019_FebyantiEirene/week10/CryptoDashboard.kt
+++ src/oop_115019_FebyantiEirene/week10/CryptoDashboard.kt
@@ -8,4 +8,7 @@ fun main() {
     coinRepo.add(Coin("BTC.", 0.5))
     coinRepo.add(Coin("ETH", 10.2))
     coinRepo.add(Coin("USDT", 500.0))
+
+    // Membungkus list koin ke dalam ApiResponse
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
