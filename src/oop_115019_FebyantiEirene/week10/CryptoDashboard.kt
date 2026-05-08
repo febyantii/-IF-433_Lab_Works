623b1f34b7708a58fc05c7a889c8db5289e8b870
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-05-08 15:02:05 +0700

    week10: (task) initialize coin repository

---
+++ src/oop_115019_FebyantiEirene/week10/CryptoDashboard.kt
@@ -0,0 +1,11 @@
+package oop_001_johnthor.week10
+
+fun main() {
+    // Checkpoint 15: Inisialisasi repository
+    val coinRepo = WalletRepository<Coin>()
+
+    // Checkpoint 16: Mengisi data dompet (minimal 3 koin)
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 10.2))
+    coinRepo.add(Coin("USDT", 500.0))
+}
