8955c8eec4edf245b8d509ca9c8a7181d09da188
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-05-08 13:59:44 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_115019_FebyantiEirene/week11/SmartHomeDashboard.kt
+++ src/oop_115019_FebyantiEirene/week11/SmartHomeDashboard.kt
@@ -3,4 +3,11 @@ package oop_115019_FebyantiEirene.week11
 fun main() {
     // Siapkan penampung perangkat
     val homeDevices = mutableListOf<SmartDevice>()
+
+    val lamp = SmartDevice("Philips WiZ Living Room", "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
