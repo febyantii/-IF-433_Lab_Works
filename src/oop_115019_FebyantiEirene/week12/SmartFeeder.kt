a9f8742d695a680c9811c576bd8f782f76d0bba4
Febyanti Eirene <eirene.febyanti@gmail.com>
2026-05-15 14:31:11 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_115019_FebyantiEirene/week12/SmartFeeder.kt
+++ src/oop_115019_FebyantiEirene/week12/SmartFeeder.kt
@@ -1,21 +1,18 @@
 package oop_001_johnthor.week12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
-    // Validasi porsi harus lebih dari 0
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
 
-    // Logika pengecekan stok dan mekanik akan ditambahkan di langkah berikutnya
-    return availableGram - requestedGram
-}
-
-fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
-    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
-
-    // Validasi Dispenser
     if (isJammed) {
         throw DispenserJamException()
     }
 
-    // Lanjut ke logika berikutnya...
-    return availableGram
+    // Validasi Stok Makanan
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
 }
+
